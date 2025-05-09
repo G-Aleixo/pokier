7a62839100627f5c280229e98609598ad2a05f09
G-Aleixo <163425479+G-Aleixo@users.noreply.github.com>
2025-05-09 01:06:38 +0000

    Add planning for packet sending structure

--- client.py
+++ client.py
@@ -8,8 +8,7 @@ def handshake(addr: socket.socket) -> dict:
         if addr.recv(1024) == b"CONFIRM":
             return True
     else:
-        return False
-    
+        return False    
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -22,5 +21,32 @@ sock.connect((address, port))
 
 if handshake(sock):
     print("Connnected")
+
+# Get input from player to show ready
+
+# Send ready
+
+# Await further instructions
+
+# All ready instruction
+
+# Ack
+
+# Game loop
+
+#   Get cards
+
+#   Bet till all pay, check or player folds
+
+#   Reveal card
+
+#   Continue till 5 cards shown
+
+#   Calc who won then everyone get everyone elses' hands shown, expect folded
+#   Maybe change in server settings
+
+#   Move bet money accordingly
+
+#   Quit or continue, just go back to 
 
 sock.close()
