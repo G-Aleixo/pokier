4b9393f81bc38d808376071056a825ba0782ebe9
G-Aleixo <163425479+G-Aleixo@users.noreply.github.com>
2025-05-09 01:58:47 +0000

    Start to implement card dealing

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import threading
+import random
 import socket
+
+card = tuple[int, int]
 
 # []
 # 2
@@ -55,18 +58,37 @@ def index2card(index: int, suit: int) -> str:
             card += "K"
         case 12:
             card += "A"
-    match suit:
+    match suit: #TODO: Add support to unicode chars later
         case 0:
-            card += "S"
+            card += "S"# chr(9828)
         case 1:
-            card += "H"
+            card += "H"# chr(9829)
         case 2:
-            card += "C"
+            card += "C"# chr(9830)
         case 3:
-            card += "D"
+            card += "D"# chr(9831)
     
     return card
 
+def print_cards(cards_list: list[card]):
+    for card in cards_list:
+        print(index2card(card[0], card[1]), end=" ")
+    print()
+
+
+def deal_cards(player_amount: int) -> tuple[list[tuple[card, card]], list[card]]:
+    card_list = [(rank, suit) for rank in range(13) for suit in range(4)]
+    random.shuffle(card_list) # in random we trust
+    
+    discard = []
+    
+    player_hands = [[] for _ in range(player_amount)]
+    
+    for i in range(player_amount * 2):
+        player_hands[i % player_amount].append(card_list.pop(0))
+        discard.append(card_list.pop(0))
+    
+    return player_hands, discard
 
 def handshake(addr: socket.socket) -> dict:
     # mock data
@@ -80,21 +102,22 @@ def handshake(addr: socket.socket) -> dict:
         return data
     else:
         return None
-    
+
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 address = "127.0.0.1"
 port = 50433
 
-max_player = 2
+max_players = 2
 player_count = 0
 
 clients = {}
 
 sock.bind((address, port))
 
-while player_count < max_player:
+while player_count < max_players:
     sock.listen()
 
     addr, port = sock.accept()
@@ -109,6 +132,16 @@ while player_count < max_player:
         player_count += 1
 
 print("All clients loaded")
+
+print("Dealing cards")
+# deal cards
+cards, discarted = deal_cards(max_players)
+
+for hand in cards:
+    print_cards(hand)
+print_cards(discarted)
+
+# TODO: Send the cards to the clients
 
 for conn in clients:
     conn.close()
